e37c6c7265420466560995e484d5382918226e46
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-11-30 20:58:55 +0800

    separate the controller

---
+++ app/controllers/users_controller.js
@@ -0,0 +1,37 @@
+var db = require('../../config/database');
+
+function getAllUsers(req, res, next) {
+  db.any('select * from users')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL users'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSingleUser(req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.one('select * from users where id = $1', pupID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE puppy'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllUsers: getAllUsers,
+  getSingleUser: getSingleUser
+};
