b46575c9136525e4639fd244b6d0e366d9cfdb20
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-11-30 20:59:40 +0800

    move out common code to a more structured layout

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var morgan = require('morgan');
+var app = express();
+
+// set the port
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+
+// db configuration
+var db = require('./config/database');
+
+// controller
+var user = require('./app/controllers/users_controller');
+
+app.get('/api/users', user.getAllUsers);
+
+app.use(morgan('dev'));
+app.listen(port);
