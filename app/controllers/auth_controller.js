e48c948fea00098262066845cdde83ab51c9c6c1
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-12-02 17:16:21 +0800

    update user account with jwt token and token verifiction

--- app/controllers/auth_controller.js
+++ app/controllers/auth_controller.js
@@ -9,6 +9,7 @@ var BearerStrategy = passportBearer.Strategy;
 var LocalStrategy = passportLocal.Strategy;
 
 passport.use(new LocalStrategy(function(username, password, done){
+
   // check to see if the username exists
   knex('users').where({ username }).first()
   .then(function(user) {
@@ -16,18 +17,30 @@ passport.use(new LocalStrategy(function(username, password, done){
     if (!authHelpers.comparePass(password, user.password)) {
       return done(null, false);
     } else {
-      return done(null, user);
+      signed_token = jwt.sign({id: user.id}, process.env.SECRET_KEY);
+      knex('users').where({ username })
+      .update({token: signed_token })
+      .then(function(){
+        return done(null, user);
+      });
     }
   })
-  .catch((err) => { return done(err); });
-
+  .catch(function(err){ return done(err); });
 }));
 
 passport.use(new BearerStrategy(function (token, done) {
   jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
     if (err) return done(err);
-    var user = users[decoded.id];
-    return done(null, user ? user : false);
+    knex('users').where({ token }).first()
+    .then(function(user){ 
+      if(!user){
+        return done(null, false);
+      }
+      else {
+        var authorized_user = user;
+        return done(null, authorized_user);
+      }
+    })
   });
 }));
 
