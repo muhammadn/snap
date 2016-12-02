7d4fefc6fe48ffa9ad3a61f3a34ebcf9021a46da
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-12-02 22:53:45 +0800

    no need to declare headers for CORS since we are using cors library

--- app.js
+++ app.js
@@ -36,14 +36,6 @@ app.use(bodyParser.json());
 app.use(morgan('dev')); // for logging requests
 
 app.post('/auth/login', authHelpers.loginRedirect, function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
 
   auth.authenticate('local', function(err, user, info) {
     if (err) { handleResponse(res, 500, 'error'); }
